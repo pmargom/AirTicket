352d8e76cc6db1718648e069a8dce42c6b2a6fd5
pmargom <pmargom@gmail.com>
2016-07-03 13:11:08 +0100

    Added multiple global settings

--- AirticketApp/Global.asax.cs
+++ AirticketApp/Global.asax.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Net.Http.Formatting;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AirticketApp.App_Start;
+using AutoMapper;
 
 namespace AirticketApp
 {
@@ -13,6 +12,7 @@ namespace AirticketApp
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c=>c.AddProfile<MappingProfile>());
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
