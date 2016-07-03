2d781275af69334b08719646c3ccb8df3791ba0e
pmargom <pmargom@gmail.com>
2016-07-03 13:10:31 +0100

    Added mapping profile for domain objecto model and dto (airport)

---
+++ AirticketApp/App_Start/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AirticketApp.Dtos;
+using AirticketApp.Models;
+using AutoMapper;
+
+namespace AirticketApp.App_Start
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Airport, AirportDto>();
+            Mapper.CreateMap<AirportDto, Airport>();
+        }
+    }
+}
