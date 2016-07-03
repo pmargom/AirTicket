bcdbc7d5c25dfae2019eef69f4d9f0f03a8f9023
pmargom <pmargom@gmail.com>
2016-07-03 10:28:48 +0100

    Populate Tables

---
+++ AirticketApp/Migrations/201607030842528_PopulateAirlineUsers.cs
@@ -0,0 +1,20 @@
+namespace AirticketApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateAirlineUsers : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO AirlineUsers (UserName, Password) VALUES ('staff1', '123456')");
+            Sql("INSERT INTO AirlineUsers (UserName, Password) VALUES ('staff2', '123456')");
+            Sql("INSERT INTO AirlineUsers (UserName, Password) VALUES ('staff3', '123456')");
+            Sql("INSERT INTO AirlineUsers (UserName, Password) VALUES ('staff4', '123456')");  
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
