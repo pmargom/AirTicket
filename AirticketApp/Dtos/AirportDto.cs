31868078897fc4827ccf83855f43e2badd5d2a90
pmargom <pmargom@gmail.com>
2016-07-03 13:10:03 +0100

    Added first dto objects (airport)

---
+++ AirticketApp/Dtos/AirportDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirticketApp.Dtos
+{
+    public class AirportDto
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(75)]
+        public string City { get; set; }
+
+        [StringLength(30)]
+        public string Country { get; set; }
+    }
+}
