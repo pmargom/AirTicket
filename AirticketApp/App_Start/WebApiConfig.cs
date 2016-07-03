9b981d4009c883e935b213ce808fb979a4a43bf4
pmargom <pmargom@gmail.com>
2016-07-03 13:09:37 +0100

    Added web api json settings

--- AirticketApp/App_Start/WebApiConfig.cs
+++ AirticketApp/App_Start/WebApiConfig.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net.Http.Headers;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace AirticketApp
 {
@@ -9,9 +9,13 @@ namespace AirticketApp
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
-            // Web API routes
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+        
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
